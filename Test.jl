c4ea35fa3ce0b62e86d4e16715518cb74cdacf11
Johan <jekbe@kth.se>
2023-03-28 17:22:18 +0200

    Generic function, section 2.5 done
    Co-authored-by: TheaNoteberg <TheaNoteberg@users.noreply.github.com>

--- Test.jl
+++ Test.jl
@@ -1,23 +1,65 @@
-# Define the macro
-# macro defclass(args...)
-#     name = args[1]
-#     superclasses = length(args[2].args) > 0 ? args[2].args : [:Any]
-#     slots = args[end]
+macro defclass(name, superclasses, slots...)
 
-#     quote
-#         struct $name <: $(superclasses...)
-#             $(Symbol.(slots)) # define the slots
-#         end
-#     end
-# end
+    superclasses_body = isempty(superclasses.args) ? [:Any] : superclasses.args
+    quote
+        struct $name <: $(superclasses_body)
+            $(Symbol(slots[1])) # define the first slot
+        end
+    end
+end
 
-# # Define a class using the macro
-# @defclass(Person, [], [name, age, profession])
+@defclass(ComplexNumber, [], [real, imag])
 
-# # Create an instance of the class
-# p = Person("John Doe", 30, "Engineer")
+function new(class, args...)
+    instance = class()
+    for (slot, value) in zip(fieldnames(class), args)
+        setfield!(instance, slot, value)
+    end
+    return instance
+end
 
-# # Test that the instance has the specified slots
-# println(p.name)        # Output: John Doe
-# println(p.age)         # Output: 30
-# println(p.profession)  # Output: Engineer
+mutable struct ComplexNumber 
+    real
+    imag
+
+    ComplexNumber() = new(0, 0)
+end
+
+
+c1 = new(ComplexNumber, real=1, imag=2)
+
+
+# Section 2.4
+struct MethodContainer
+    parameters::Tuple
+    method::Function
+end
+
+struct GenericFunction
+    name::Symbol
+    methods::Dict{Int, Array{MethodContainer, 1}}
+end
+
+add_generic = GenericFunction(:add, Dict())
+method = MethodContainer((Int, Int), 
+function add(a::Int, b::Int)
+    return a + b
+end)
+
+if !haskey(add_generic.methods, 2)
+    add_generic.methods[2] = Array{MethodContainer, 1}()
+end
+push!(add_generic.methods[2], method)
+
+if !haskey(add_generic.methods, 3)
+    add_generic.methods[3] = Array{MethodContainer, 1}()
+end
+
+push!(add_generic.methods[3], MethodContainer((Int, Int, Int), 
+function add(a::Int, b::Int, c::Int)
+    return (a + b)*c
+end))
+
+add_generic.methods[2]
+
+# Section 2.5
