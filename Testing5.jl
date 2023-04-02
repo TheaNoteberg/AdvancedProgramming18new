860d957f294a69b11ef7fd0223d00a8555606479
Johan <jekbe@kth.se>
2023-04-02 19:49:29 +0200

    CPL done and sorting of methods

--- Testing5.jl
+++ Testing5.jl
@@ -10,6 +10,7 @@ Class.slots[:direct_slots] = [:name, :direct_superclasses, :direct_slots]
 Top = Instance(Dict{Symbol, Any}())
 Top.slots[:name] = :Top
 Top.slots[:instance_of] = Class
+Top.slots[:direct_superclasses] = []
 
 Object = Instance(Dict{Symbol, Any}())
 Object.slots[:name] = :Object
@@ -18,13 +19,43 @@ Object.slots[:instance_of] = Class
 Class.slots[:direct_superclasses] = [Object]
 Object.slots[:direct_superclasses] = [Top]
 
+function Base.getproperty(instance::Instance, get::Symbol)
+    all_slots = getfield(instance,:slots)
+    if get == :slots
+        if haskey(all_slots, :direct_slots)
+            return all_slots[:direct_slots]
+        else
+            error("ERROR: Slot $(slot) is missing\n...")
+        end
+    end
+    if haskey(all_slots, get)
+        if !isnothing(all_slots[get])
+            return all_slots[get]
+        else
+            error("ERROR: Slot $(slot) is unbound\n...")
+        end
+    else
+        error("ERROR: Slot $(slot) is missing\n...")
+    end
+end
+
+function Base.setproperty!(instance::Instance, set::Symbol, val::Any)
+    all_slots = getfield(instance,:slots)
+    if haskey(all_slots, set)
+        all_slots[set] = val
+    else
+        error("ERROR: Slot $(slot) is missing\n...")
+    end
+end
+
 function new(class::Instance, ;kwargs...)
     slots_from_class = getfield(class, :slots)[:direct_slots]
     slots = Dict{Symbol, Any}()
     for (first, second) in kwargs
         if first in slots_from_class
             slots[first] = second
-            if first == :direct_superclasses && class == Class && !isempty(second)
+            if first == :direct_superclasses && class == Class && isempty(second)
+                print("object")
                 slots[first] = [Object]
             end
         else
@@ -42,6 +73,7 @@ macro defclass(classname, superclasses, slots)
 end
 
 @defclass(ComplexNumber, [], [real, imag])
+ComplexNumber.direct_superclasses == [Object]
 
 GenericFunction = new(Class, name=:GenericFunction, direct_superclasses=[Object], direct_slots=[:name, :methods, :number_of_args])
 MultiMethod = new(Class, name=:MultiMethod, direct_superclasses=[Object], direct_slots=[:specializers, :procedure, :generic_function])
@@ -58,17 +90,18 @@ macro defgeneric(x)
 end
 @defgeneric add(a, b)
 
-function (g::Instance)(args...)
-    slots = getfield(g, :slots) 
+function (inst::Instance)(args...)
+    slots = getfield(inst, :slots) 
     if slots[:instance_of] === GenericFunction
         procedure = getfield(slots[:methods][1], :slots)[:procedure]
+        # [:specializers, :procedure, :generic_function]
+        for method in slots[:methods]
+            if check_args(method, args...)
+                return method.procedure(args...)
+            end
+        end
+        error("ERROR: No method for $(g) with arguments $(args)\n...")
         return procedure(args...)
-        #for method in slots[:methods]
-        #    if check_args(method, args)
-        #        return method.procedure(args...)
-        #    end
-        #end
-        #error("ERROR: No method for $(g) with arguments $(args)\n...")
     else
         error("ERROR: $(g) is not a generic function\n...")
     end
@@ -100,65 +133,68 @@ macro defmethod(x)
     end
 end
 
-function Base.getproperty(instance::Instance, get::Symbol)
-    all_slots = getfield(instance,:slots)
-    if get == :slots
-        if haskey(all_slots, :direct_slots)
-            return all_slots[:direct_slots]
-        else
-            error("ERROR: Slot $(slot) is missing\n...")
-        end
-    end
-    if haskey(all_slots, get)
-        if !isnothing(all_slots[get])
-            return all_slots[get]
-        else
-            error("ERROR: Slot $(slot) is unbound\n...")
-        end
-    else
-        error("ERROR: Slot $(slot) is missing\n...")
-    end
+function class_of(inst::Instance)
+    return getfield(inst, :slots)[:instance_of]
 end
 
-function Base.setproperty!(instance::Instance, set::Symbol, val::Any)
-    all_slots = getfield(instance,:slots)
-    if haskey(all_slots, set)
-        all_slots[set] = val
-    else
-        error("ERROR: Slot $(slot) is missing\n...")
+# Gets the class precedence list with BFS
+function compute_cpl(class::Instance)
+    visited = []
+    queue = [class]
+    push!(visited, class)
+    while !isempty(queue)
+        node = queue[1]
+        deleteat!(queue, 1)
+        neighbors = getfield(node, :slots)[:direct_superclasses]
+        for neighbor in neighbors
+            if !(neighbor in visited)
+                push!(queue, neighbor)
+                push!(visited, neighbor)
+            end
+        end
     end
+    return visited
 end
-GenericFunction = new(Class, name=:GenericFunction, direct_superclasses=[Object], direct_slots=[:name, :methods, :number_of_args])
-MultiMethod = new(Class, name=:MultiMethod, direct_superclasses=[Object], direct_slots=[:specializers, :procedure, :generic_function])
-add1 = new(MultiMethod, specializers=[Int, Int], procedure=(a, b) -> a + b, generic_function=add)
-push!(add.methods, add1)
-add.methods[1]
 
-function check_args(method::MultiMethod, args::Vector)
-    if length(args) != length(method.specializers)
-        return false
-    end
-    for i in 1:length(args)
-        if args[i] != method.specializers[i]
-            return false
+function is_more_specific(method1::Instance, method2::Instance, args)
+    for i in eachindex(args)
+        class1 = method1.specializers[i].second
+        class2 = method2.specializers[i].second
+        if  class1 != class2
+            actual_class = args[i]
+            precedence_list = compute_cpl(actual_class)
+            return findfirst(x -> x == class1, precedence_list) < findfirst(x -> x == class2, precedence_list)
         end
     end
     return true
 end
 
-function run_generic_function(f::GenericFunction, args::Vector)
+function find_applicable_methods(methods::Array, args...)
     applicable_methods = []
-    for method in f.methods
-        if check_args(method, args)
+    for method in methods
+        applicable = true
+        for i in eachindex(args)
+            cpl = compute_cpl(args[i])
+            class = method.specializers[i].second
+            if !(class in cpl)
+                applicable = false
+                break
+            end
+        end
+        if applicable
             push!(applicable_methods, method)
         end
-    end
-    # find most applicable_methods
-    if length(applicable_methods) == 0
-        error("No applicable method found")
-    end
+	end
+	return sort(applicable_methods, lt=(x,y)->is_more_specific(x, y, args))
 end
 
-function class_of(inst::Instance)
-    return getfield(inst, :slots)[:instance_of]
+@defclass(A, [], [])
+@defclass(B, [], [])
+@defclass(C, [], [])
+@defclass(D, [A, B], [])
+@defclass(E, [A, C], [])
+@defclass(F, [D, E], [])
+
+for x in compute_cpl(F)
+    println(getfield(x, :slots)[:name])
 end
